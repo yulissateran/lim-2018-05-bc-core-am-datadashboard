0077959b0b9d78c0b4776cccd7497c8cf896768e
yulissateran <yulissa.lteran@gmail.com>
2018-06-13 20:18:47 -0500

    Subiendo código para filtrar alumnas por sede

--- src/data.js
+++ src/data.js
@@ -1,57 +1,106 @@
+//Declaración de variables
+const testBtnLima = document.getElementById('testBtnLima');
+const testBtnChile = document.getElementById('testBtnChile');
+const testBtnMexico = document.getElementById('testBtnMexico');
+const searchBtn = document.getElementById('search');
+let enterName = document.getElementById('enter-name');
+const cohortLima = document.getElementById('cohort-Lima');
+const getUsersEndpoints = '/data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const getUsersProgressEndpoint = '/data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const getUsersGlobalEndpoint = '/data/cohorts.json';
+const resultUsers = document.getElementById('response-container');
+const filterAlumnas = document.getElementsByClassName('search-alumnas')[0];
+const selector = document.getElementById('selector');
 
+//Lista de Cohorts
+const getCohorts = () => {
+  let filtrarIdCohorts = [];
+  fetch(getUsersGlobalEndpoint, { method: 'GET' })
+    .then((response) => {
+      if (response.status !== 200) {
+        alert('Error')
+      }
+      return response.json();
+    })
+    .then((dataCohorts) => {
+      dataCohorts.forEach(cohort => {
+        let listIdCohorts = document.createElement('option');
+        listIdCohorts.innerText = cohort.id;
+        selector.appendChild(listIdCohorts);
+      })
+    })
+}
 
-let usuariasDelCohort = "/data/cohorts/lim-2018-03-pre-core-pw/users.json";
-const progresoDeUsuarias = "/data/cohorts/lim-2018-03-pre-core-pw/progress.json"
-const listaDeCohorts = "/data/cohorts.json";
-
-let usuarias ="";
-let method= "GET";
-let traerDatos = ()=>{ 
-  fetch(usuariasDelCohort)
-  .then((response) => response.json())
-  .then((data) => data)
-  .catch((err)=> {
-  console.log(err.message)
-  });
-
+// FILTER STUDENTS BY ZONE
+const getUsers = (timezone) => {
+  let filterUsers = [];
+  fetch(getUsersEndpoints, { method: 'GET' })
+    .then((response) => {
+      if (response.status !== 200) {
+        alert('Error')
+      }
+      return response.json();
+    })
+    .then((users) => {
+      users.forEach(user => {
+        if (user.timezone === timezone) {
+          filterUsers.push(user);
+        }
+      });
+      debugger
+      listUsers(filterUsers);
+    })
 };
-document.getElementById('SalidadeDatos').innerHTML = traerDatos();
-// console.log(traerDatos(usuariasDelCohort));
-document.getElementById('plasmarDatos').addEventListener('click', traerDatos(usuariasDelCohort));
 
-// const traerprogreso = () => {
-//   fetch(progresoDeUsuarias)
-//     .then((response) => response.json())
-//     .then((progres) => {
-//       console.log(progres);
-//     }).catch((err) => {
-//       console.log(err.message);
-//     });
-// };
-// document.getElementById('plasmarProgreso').addEventListener('click', traerprogreso()); 
+const listUsers = (lstUsers) => {
+  resultUsers.innerHTML = '';
+  lstUsers.forEach(user => {
+    let liUser = document.createElement('li');
+    liUser.innerText = user.name;
+    resultUsers.appendChild(liUser);
+  });
+}
 
-// const traercohorts = () => {
-//   fetch(listaDeCohorts)
-//     .then((response) => response.json())
-//     .then((cohorts) => {
-//       //  procesandorespuesta(data);
-//       //  arreglo.push(data);
-//       // console.log(progres["00hJv4mzvqM3D9kBy3dfxoJyFV82"]);
-//       console.log(JSON.stringify(cohorts));
-//       console.log(cohorts[0]);
-//     }).catch((err) => {
-//       console.log(err.message);
-//     });
-// };
+testBtnLima.addEventListener('click', (e) => {
+  e.preventDefault();
+  //getUsers('America/Lima');
+  filterAlumnas.classList.remove('hidden');
+  getCohorts();
+});
 
-// document.getElementById('plasmarProgreso').addEventListener('click', traercohorts()); 
-// fetch('https://api-to-call.com/endpoint')
-//   .then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-//     throw new Error('Request failed!');
-//   }, networkError => console.log(networkError.message)
-//   ).then(jsonResponse => {
-//     return jsonResponse;
-//   });
+testBtnChile.addEventListener('click', (e) => {
+  e.preventDefault();
+  getUsers('America/Chile');
+});
+
+testBtnMexico.addEventListener('click', (e) => {
+  e.preventDefault();
+  getUsers('America/Mexico');
+
+});
+
+
+// FILTER STUDENTS BY NAME
+const FilterUsers = (users) => {
+  let filterUsersName = [];
+  fetch(getUsersEndpoints, { method: 'GET' })
+    .then((response) => {
+      if (response.status !== 200) {
+        alert('Error')
+      }
+      return response.json();
+    })
+    .then((requestusers) => {
+      requestusers.forEach(user => { // recorrer la data
+        if (user.name === users) {
+          filterUsersName.push(user); //adicionar elemento al array
+        }
+      });
+      listUsers(filterUsersName);
+    })
+};
+
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  FilterUsers(enterName.value);
+});
