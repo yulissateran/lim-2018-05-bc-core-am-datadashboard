fb6cd66f5d8824872a0542df8489f3992d9d4d7c
dcrisanto <dorelly.crisanto@gmail.com>
2018-06-19 17:37:42 -0500

    Función de quiz

--- src/data.js
+++ src/data.js
@@ -1,97 +1,139 @@
+//Declarando variables 
+const CohortsOfLaboratoria = '../data/cohorts.json';
+const cohortLim2018_03_precore_pw = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progressOfUsersOfLim2018_03_precore_pw = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+
 //Creando la lista de cohorts 
 const getListOfCohorts = () => {
     fetch(CohortsOfLaboratoria, { method: 'GET' })
-    .then((response) => {
-      if (response.status !== 200) {
-        alert('Error')
-      }
-      return response.json();
-      })
-   .then((responseFromCohorts) => {
-      responseFromCohorts.forEach(cohort => {
-        let nameOfCohort = document.createElement('option');
-        nameOfCohort.value = cohort.id;
-        nameOfCohort.innerText = cohort.id;
-        selectorOfCohorts.appendChild(nameOfCohort);
-      })
-    })
-  }
-  // Filtrando estudiantes por cohort
-  const getNameUsersOfCohort = (NameOfCohort) => {
+        .then((response) => {
+            if (response.status !== 200) {
+                alert('Error')
+            }
+            return response.json();
+        })
+        .then((responseCohorts) => {
+            responseCohorts.forEach(cohort => {
+                let nameOfCohort = document.createElement('option');
+                nameOfCohort.value = cohort.id;
+                nameOfCohort.innerText = cohort.id;
+                selectorOfCohorts.appendChild(nameOfCohort);
+            })
+        })
+}
+// Filtrando estudiantes por cohort
+const getUsersOfCohort = (nameOfCohort) => {
     let arrayOfUsersOfOneCohort = [];
     fetch(cohortLim2018_03_precore_pw, { method: 'GET' })
-      .then((response) => {
-        if (response.status !== 200) {
-          alert('Error')
-        }
-        return response.json();
+        .then((response) => {
+            if (response.status !== 200) {
+                alert('Error')
+            }
+            return response.json();
         })
-      .then((responseOfCohort) => {
-        responseOfCohort.forEach(user => {
-          if (user.signupCohort === NameOfCohort) {
-            arrayOfUsersOfOneCohort.push(user);
-          }
-        });
-        paintUsersFromCohort(arrayOfUsersOfOneCohort);
-      })
-  }; 
-  //pinta la ista de usuarias del cohort y le da un "enlace" hacia progres
-  const paintUsersFromCohort = (arrayOfUsersOfOneCohort) => {
+        .then((responseUsersOfCohort) => {
+            responseUsersOfCohort.forEach(user => {
+                if (user.signupCohort === nameOfCohort) {
+                    arrayOfUsersOfOneCohort.push(user);
+                }
+            });
+            paintUsersCohort(arrayOfUsersOfOneCohort);
+        })
+};
+// Pinta la ista de usuarias del cohort y le da un "enlace" hacia progress
+const paintUsersCohort = (arrayOfUsersOfOneCohort) => {
     containerListUsers.innerHTML = '';
     arrayOfUsersOfOneCohort.forEach(user => {
-      let createElementLi = document.createElement('li');
-      let createElement_A = document.createElement('a');
-      createElement_A.innerHTML = user.name,
-      createElement_A.setAttribute('href', 'javascript;');
-      createElement_A.addEventListener('click', (e)=>{
-        e.preventDefault();
-        getUsersProgress(user.id);
-      });
-      createElementLi.appendChild(createElement_A);
-      containerListUsers.appendChild(createElementLi);
-      });
-  }
-  //Buscar estudiantes por nombre
-  const searchStudent = (student) => {
-    let arrayNameUser = [];
-    fetch(cohortLim2018_03_precore_pw, { method: 'GET' })
-    .then((response) => {
-      if (response.status !== 200) {
-        alert('Error')
-      }
-      return response.json();
-      })
-      .then((dataOfUsers) => {
-        dataOfUsers.forEach(user => { // recorrer la data
-          if (user.name === student) {
-            arrayNameUser.push(user); //adicionar elemento al array
-          }
+        let createElementLi = document.createElement('li');
+        let createElement_A = document.createElement('a');
+        createElement_A.innerHTML = user.name,
+            createElement_A.setAttribute('href', 'javascript;');
+        createElement_A.addEventListener('click', (e) => {
+            e.preventDefault();
+            getUsersProgress(user.id);
         });
-        paintUsersFromCohort(arrayNameUser);
-      })
-  };
-  //Traer progreso
-  const getUsersProgress = (idStudent) => {
+        createElementLi.appendChild(createElement_A);
+        containerListUsers.appendChild(createElementLi);
+    });
+}
+//Traer progreso
+const getUsersProgress = (idStudent) => {
     fetch(progressOfUsersOfLim2018_03_precore_pw, { method: 'GET' })
-    .then((response) => {
-      if (response.status !== 200) {
-        alert('Error')
-      }
-      return response.json();
-      })
-      .then((progressStudents) => {
-        let progressUser = progressStudents[idStudent]["intro"]["percent"];
-        createContainerForScore(progressUser)
-         console.log(progressUser);
-      })
-  };
-  const createContainerForScore=(scoreForStudent)=>{
-    containerListProgress.innerHTML="";
+        .then((response) => {
+            if (response.status !== 200) {
+                alert('Error')
+            }
+            return response.json();
+        })
+        .then((progressStudents) => {
+            let progressUser = progressStudents[idStudent];
+            deagregateTypes(progressUser);
+        })
+};
+
+const deagregateTypes = (progressUser) => {
+    let intro = progressUser['intro'];
+    //contadores quizzes
+    let quizCounter = 0;
+    let completedQuizCounter = 0;
+    let quizAccumulatedScore = 0;
+    //contadores read
+    let readCounter = 0;
+    let completedReadCounter = 0;
+    //contadores exercises
+    let exercisesCounter = 0;
+    let completedExercisesCounter = 0;
+
+    //recorremos las unidades
+    for (unitKey in intro.units) {
+        let unit = intro.units[unitKey];
+        for (partKey in unit.parts) {
+            let part = unit.parts[partKey];
+            let type = part.type;
+            if (type === 'quiz') {
+                quizCounter++;
+                if (part.completed === 1) {
+                    completedQuizCounter++;
+                    quizAccumulatedScore = quizAccumulatedScore+part.score;
+                }
+            }
+        }
+    }
+
+    console.log(quizCounter);
+    console.log(completedQuizCounter);
+    console.log(quizAccumulatedScore);
+
+};
+
+const createContainerForScore = (scoreForStudent) => {
+    containerListProgress.innerHTML = "";
     let createElement_Li = document.createElement('li');
     createElement_Li.innerText = scoreForStudent;
     let createElementP = document.createElement('p');
     createElementP.innerText = "Porcentaje de completidud de todos los cursos";
     containerListProgress.appendChild(createElementP);
     containerListProgress.appendChild(createElement_Li);
-    
-  }
+
+}
+
+//Buscar estudiantes por nombre
+const searchStudent = (student) => {
+    let arrayNameUser = [];
+    fetch(cohortLim2018_03_precore_pw, { method: 'GET' })
+        .then((response) => {
+            if (response.status !== 200) {
+                alert('Error')
+            }
+            return response.json();
+        })
+        .then((dataOfUsers) => {
+            dataOfUsers.forEach(user => { // recorrer la data
+                if (user.name === student) {
+                    arrayNameUser.push(user); //adicionar elemento al array
+                }
+            });
+            paintUsersFromCohort(arrayNameUser);
+        })
+};
+
--- src/main.js
+++ src/main.js
@@ -1,30 +1,28 @@
-//declarando variables 
-const CohortsOfLaboratoria = '../data/cohorts.json';
-const cohortLim2018_03_precore_pw = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const progressOfUsersOfLim2018_03_precore_pw = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+//Declarando variables 
+const selectorOfCohorts = document.getElementById('selector-Of-Cohorts');
 const containerListUsers = document.getElementById('containerListUsers');
 const containerListProgress = document.getElementById('containerListProgress');
-const selectorOfCohorts = document.getElementById('selectorOfCohorts');
-//arreglar variables
 const cohortsBtn = document.getElementById('cohorts-btn');
 const titleListStudent = document.getElementById('list-students');
 const searchStudents = document.getElementById('search-students');
 const btnSearch = document.getElementById('btnSearch'); 
 let searchName = document.getElementById('searchName');
+
 // Evento para el boton cohorts que liste los cohorts
-cohortsBtn.addEventListener('click', (e) => {
+// DOMContentLoaded: Cuando el HTML termina de cargar
+document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
-    selectorOfCohorts.classList.remove('hidden');
-    
+    //se llenan las opciones
     getListOfCohorts();
-});
-// Evento para selector, que crea la llista de alumnas según el cohort
+})
+// Evento para selector, que crea la lista de alumnas según el cohort
 selectorOfCohorts.addEventListener('change', (e) => {
     e.preventDefault();
-    getNameUsersOfCohort(selectorOfCohorts.value);
+    getUsersOfCohort(selectorOfCohorts.value);
     searchStudents.classList.remove('hidden');
     titleListStudent.classList.remove('hidden');
 });
+//Evento para el boton, que busca las estudiantes por nombre completo
 btnSearch.addEventListener('click', (e) => {
         e.preventDefault();
         searchStudent(searchName.value);
