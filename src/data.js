e4ea66346060250d32206233dcb157e720f3a299
yulissateran <yulissa.lteran@gmail.com>
2018-06-21 22:43:43 -0500

    subiendo bucle for of que pinta las usuarias y sus porcentajes en la pantalla

--- src/data.js
+++ src/data.js
@@ -1,193 +1,95 @@
-//Creando la lista de cohorts 
-const getListOfCohorts = () => {
-    fetch(CohortsOfLaboratoria, { method: 'GET' })
-        .then((response) => {
-            if (response.status !== 200) {
-                alert('Error')
+window.data = {
+  computeUsersStats(users, progress, courses) {
+    const usersWithStats = users;
+    const keysProgress = Object.keys(progress);
+    let scoreOfCohort =0;
+    let promedioPercentOfQuizzs='';
+
+    let studentQuantiti = 0;
+   try{ 
+      for (const user of usersWithStats){
+       for(const id of keysProgress){
+          if(user.id === id && user.role === 'student'){
+            let quizzsTotal = 0;
+            let quizzCompleted = 0;
+            let readsTotals = 0;
+            let readsCompleted = 0;
+            studentQuantiti++
+            if(Object.entries(progress[id]).length !== 0){
+              const unitsOfCourses = progress[id][courses].units;  // console.log(unitsOfCourses)
+              for (unit in unitsOfCourses) {
+               
+                let partsOfUnit = unitsOfCourses[unit].parts;       //console.log(partsOfUnit)
+                for (part in partsOfUnit) {
+                  let propertysOfTheParts = partsOfUnit[part]; //console.log(propertysOfTheParts.completed)
+                  let type = propertysOfTheParts.type;              //console.log(type)
+                  if (type === 'quiz' ) {
+                    quizzsTotal++
+                    if (propertysOfTheParts.completed === 1) { quizzCompleted++ }
+                  }
+                  //  if (type === 'practice') {  // console.log(propertysOfTheParts.completed);
+                  //  if (propertysOfTheParts.completed === 1) { } }
+                    if (type === 'read' ) {
+                      readsTotals++
+                      if (propertysOfTheParts.completed === 1) { readsCompleted++ }
+                 }
+               }
+             } 
+             user['stats'] = {};
+             user.stats['reads'] = {};
+              user.stats.reads['total'] = readsTotals;
+              user.stats.reads['completed'] = readsCompleted;
+              user.stats.reads['percent'] = (readsCompleted / readsTotals)*100;
+             user.stats['quizzes'] = {};
+              user.stats.quizzes['total'] = quizzsTotal;
+              user.stats.quizzes['completed'] = quizzCompleted;
+              user.stats.quizzes['percent'] = (quizzCompleted / quizzsTotal)*100;
+             user.stats['percent'] = progress[id][courses]['percent'];
+             user.stats['exercises'] = {};
+              user.stats.exercises['total'] = (Object.keys(unitsOfCourses['02-variables-and-data-types']['parts']['06-exercises']['exercises'])).length;
+              user.stats.exercises['completed'] = unitsOfCourses['02-variables-and-data-types']['parts']['06-exercises']['exercises']['01-coin-convert']['completed'] +
+                unitsOfCourses['02-variables-and-data-types']['parts']['06-exercises']['exercises']['02-restaurant-bill']['completed'];
+              user.stats.exercises['percent'] = ((unitsOfCourses['02-variables-and-data-types']['parts']['06-exercises']['completed']) * 100);
+
+
+            // user.stats['reads']= progress[id][courses]['percent'];
+            } else if (Object.entries(progress[id]).length === 0){
+                  
+             // console.log(user)
+              user['stats'] = {};
+              user.stats['reads'] = {};
+              user.stats.reads['total'] = 0;
+              user.stats.reads['completed'] = 0;
+              user.stats.reads['percent'] = 0;
+              user.stats['quizzes'] = {};
+              user.stats.quizzes['total'] = 0;
+              user.stats.quizzes['completed'] = 0;
+              user.stats.quizzes['percent'] = 0;
+              user.stats['percent'] = 0;
+              user.stats['exercises'] = {};
+              user.stats.exercises['total'] = 0;
+              user.stats.exercises['completed'] = 0;
+              user.stats.exercises['percent'] = 0;
             }
-            return response.json();
-        })
-        .then((responseCohorts) => {
-            responseCohorts.forEach(cohort => {
-                let nameOfCohort = document.createElement('option');
-                nameOfCohort.value = cohort.id;
-                nameOfCohort.innerText = cohort.id;
-                selectorOfCohorts.appendChild(nameOfCohort);
-            })
-        })
+            scoreOfCohort += user.stats.exercises.percent;
+          } 
+         
+        }
+      }
+      console.log(usersWithStats.length);
+      console.log(studentQuantiti);
+     console.log(scoreOfCohort /usersWithStats.length);
+     console.log(scoreOfCohort / studentQuantiti);
+     
+     promedioPercentOfQuizzs = scoreOfCohort / studentQuantiti;
+     containerOfPercentGeneral = document.getElementById('container-of-percent-general');
+     containerOfPercentGeneral.innerHTML = 'porcentaje general de ejercicios:   ' + promedioPercentOfQuizzs;
+      //  console.log(scoreOfCohort);
+     console.log(promedioPercentOfQuizzs) 
+    }catch(err){console.log(err.message) }
+    console.log(usersWithStats)
+    return usersWithStats; 
+  },
+  dato() { }
 }
-// Filtrando estudiantes por cohort
-const getUsersOfCohort = (nameOfCohort) => {
-    let arrayOfUsersOfOneCohort = [];
-    fetch(cohortLim2018_03_precore_pw, { method: 'GET' })
-        .then((response) => {
-            if (response.status !== 200) {
-                alert('Error')
-            }
-            return response.json();
-        })
-        .then((responseUsersOfCohort) => {
-            responseUsersOfCohort.forEach(user => {
-                if (user.signupCohort === nameOfCohort) {
-                    arrayOfUsersOfOneCohort.push(user);
-                }
-            });
-            paintUsersCohort(arrayOfUsersOfOneCohort);
-        })
-};
-// Pinta la ista de usuarias del cohort y le da un "enlace" hacia progress
-const paintUsersCohort = (arrayOfUsersOfOneCohort) => {
-    containerListUsers.innerHTML = '';
-    arrayOfUsersOfOneCohort.forEach(user => {
-        let createElementLi = document.createElement('li');
-        let createElement_A = document.createElement('a');
-        createElement_A.innerHTML = user.name,
-            createElement_A.setAttribute('href', 'javascript;');
-        createElement_A.addEventListener('click', (e) => {
-            e.preventDefault();
-            getUsersProgress(user.id);
-            prueba.classList.remove('hidden');
-        });
-        createElementLi.appendChild(createElement_A);
-        containerListUsers.appendChild(createElementLi);
-    });
-}
-//Traer progreso
-const getUsersProgress = (idStudent) => {
-    fetch(progressOfUsersOfLim2018_03_precore_pw, { method: 'GET' })
-        .then((response) => {
-            if (response.status !== 200) {
-                alert('Error')
-            }
-            return response.json();
-        })
-        .then((progressStudents) => {
-            let progressUser = progressStudents[idStudent];
-            deagregateTypes(progressUser);
-        })
-};
-
-const deagregateTypes = (progressUser) => {
-    let intro = progressUser['intro'];
-    //contadores quizzes
-    let quizCounter = 0;
-    let completedQuizCounter = 0;
-    let quizAccumulatedScore = 0;
-    //contadores read
-    let readCounter = 0;
-    let completedReadCounter = 0;
-    //contadores exercises
-    let exercisesCounter = 0;
-    let completedExercisesCounter = 0;
-
-    try {
-        //recorremos las unidades
-        for (unitKey in intro.units) {
-            let unit = intro.units[unitKey];
-            for (partKey in unit.parts) {
-                let part = unit.parts[partKey];
-                let type = part.type;
-                //Datos de Quiz
-                if (type === 'quiz') {
-                    quizCounter++;
-                    if (part.completed === 1) {
-                        completedQuizCounter++;
-                        quizAccumulatedScore = quizAccumulatedScore + part.score;
-                    }
-                }
-                //Datos de exercises
-                if (type === 'practice') {
-                    exercisesCounter++;
-                    if (part.completed === 1) {
-                        completedExercisesCounter = completedExercisesCounter + part.completed;
-                    }
-                }
-                //Datos de read
-                if (type === 'read') {
-                    readCounter++;
-                    if (part.completed === 1) {
-                        completedReadCounter++;
-                    }
-                }
-                //Imprimiendo datos de Read
-                readCounterCohort.innerHTML = "";
-                completedReadStudent.innerHTML = "";
-                percentReadStudent.innerHTML = "";
-                let liReadCounter = document.createElement('li');
-                let licompletedRead = document.createElement('li');
-                let lipercentRead = document.createElement('li');
-                liReadCounter.innerHTML = readCounter;
-                readCounterCohort.appendChild(liReadCounter);
-                licompletedRead.innerHTML = completedReadCounter;
-                completedReadStudent.appendChild(licompletedRead);
-                lipercentRead.innerHTML = (completedReadCounter / readCounter) * 100;
-                percentReadStudent.appendChild(lipercentRead);
-                //Imprimiendo datos de Exercises
-                exercisesCounterCohort.innerHTML = "";
-                completedExercisesStudent.innerHTML = "";
-                percentExercisesStudent.innerHTML = "";
-                let liExercisesCounter = document.createElement('li');
-                let licompletedExercises = document.createElement('li');
-                let lipercentExercises = document.createElement('li');
-                liExercisesCounter.innerHTML = exercisesCounter;
-                exercisesCounterCohort.appendChild(liExercisesCounter);
-                licompletedExercises.innerHTML = completedExercisesCounter;
-                completedExercisesStudent.appendChild(licompletedExercises);
-                lipercentExercises.innerHTML = (completedExercisesCounter / exercisesCounter) * 100;
-                percentExercisesStudent.appendChild(lipercentExercises);
-                //Imprimiendo datos de Quiz
-                quizCounterCohort.innerHTML = "";
-                completedQuizStudent.innerHTML = "";
-                quizScoreStudent.innerHTML = "";
-                percentScoreStudent.innerHTML = "";
-                let liQuizCounter = document.createElement('li');
-                let liCompletedQuiz = document.createElement('li');
-                let liQuizScore = document.createElement('li');
-                let liPercentScore = document.createElement('li');
-                liQuizCounter.innerHTML = quizCounter;
-                quizCounterCohort.appendChild(liQuizCounter);
-                liCompletedQuiz.innerHTML = completedQuizCounter;
-                completedQuizStudent.appendChild(liCompletedQuiz);
-                liQuizScore.innerHTML = quizAccumulatedScore;
-                quizScoreStudent.appendChild(liQuizScore);
-                liPercentScore.innerHTML = (quizAccumulatedScore / quizCounter);
-                percentScoreStudent.appendChild(liPercentScore);
-                
-            }
-        } 
-    } catch (error) {
-        console.log('Data Incompleta');
-    }
-};
-
-const createContainerForScore = (scoreForStudent) => {
-    containerListProgress.innerHTML = "";
-    let createElement_Li = document.createElement('li');
-    createElement_Li.innerText = scoreForStudent;
-    let createElementP = document.createElement('p');
-    createElementP.innerText = "Porcentaje de completidud de todos los cursos";
-    containerListProgress.appendChild(createElementP);
-    containerListProgress.appendChild(createElement_Li);
-
-}
-
-//Buscar estudiantes por nombre
-const searchStudent = (student) => {
-    let arrayNameUser = [];
-    fetch(cohortLim2018_03_precore_pw, { method: 'GET' })
-        .then((response) => {
-            if (response.status !== 200) {
-                alert('Error')
-            }
-            return response.json();
-        })
-        .then((dataOfUsers) => {
-            dataOfUsers.forEach(user => { // recorrer la data
-                if (user.name === student) {
-                    arrayNameUser.push(user); //adicionar elemento al array
-                }
-            });
-            paintUsersCohort(arrayNameUser);
-        })
-};
 
--- src/main.js
+++ src/main.js
@@ -1,46 +1,63 @@
-//Declarando variables 
-const CohortsOfLaboratoria = '../data/cohorts.json';
-const cohortLim2018_03_precore_pw = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const progressOfUsersOfLim2018_03_precore_pw = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const prueba=document.getElementById('prueba');
-const selectorOfCohorts = document.getElementById('selector-Of-Cohorts');
-const containerListUsers = document.getElementById('containerListUsers');
-const containerListProgress = document.getElementById('containerListProgress');
 const cohortsBtn = document.getElementById('cohorts-btn');
 const titleListStudent = document.getElementById('list-students');
 const searchStudents = document.getElementById('search-students');
 const btnSearch = document.getElementById('btnSearch'); 
 let searchName = document.getElementById('searchName');
-const readCounterCohort = document.getElementById('read-counter');
-const completedReadStudent = document.getElementById('completed-read');
-const percentReadStudent = document.getElementById('percent-read');
-const exercisesCounterCohort = document.getElementById('exercises-counter');
-const completedExercisesStudent = document.getElementById('completed-exercises');
-const percentExercisesStudent = document.getElementById('percent-exercises');
-const quizCounterCohort = document.getElementById('quiz-counter');
-const completedQuizStudent = document.getElementById('completed-quiz');
-const quizScoreStudent = document.getElementById('quiz-score');
-const percentScoreStudent = document.getElementById('percent-score');
-
 // Evento para el boton cohorts que liste los cohorts
-// DOMContentLoaded: Cuando el HTML termina de cargar
-document.addEventListener('DOMContentLoaded', (e) => {
+cohortsBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    //se llenan las opciones
-    getListOfCohorts();
-})
+    selectorOfCohorts.classList.remove('hidden');
+    
+     getListOfCohorts();
+});
 // Evento para selector, que crea la lista de alumnas según el cohort
 selectorOfCohorts.addEventListener('change', (e) => {
     e.preventDefault();
-    getUsersOfCohort(selectorOfCohorts.value);
-    searchStudents.classList.remove('hidden');
-    titleListStudent.classList.remove('hidden');
+    if (selectorOfCohorts.value === 'lim-2018-03-pre-core-pw'){
+        getNameUsersOfCohort(selectorOfCohorts.value);
+        // getNamesOfCoursesByCohort(selectorofCohorts.value)
+        searchStudents.classList.remove('hidden');
+        titleListStudent.classList.remove('hidden');}else{
+            alert('aún no hay datos de este cohort')
+        }
+ 
 });
-//Evento para el boton, que busca las estudiantes por nombre completo
+//evento para
 btnSearch.addEventListener('click', (e) => {
         e.preventDefault();
         searchStudent(searchName.value);
     });
 
+// yuli recuerda guardar este ejemplo
+// const perros = [
+//     {
+//         name : 'shiro',
+//         edad : 13,
+//     },
+//     {
+//         name : 'boby',
+//         edad : 12,
+//     }
+// ];
 
+// const dataComidas = {
+//     shiro : {
+//         comida : 'ricocan',
+//         precio : 10
+//     },
+//     boby : {
+//         comida : 'mimaskot',
+//         precio : 19
+//     }
+// }
 
+// const keyDataComidas = Object.keys(dataComidas);
+
+// for(const perro of perros) {
+//     console.log(perro)
+//     for (const namePerro of keyDataComidas) {
+//          if (perro.name === namePerro) {
+//              console.log(dataComidas[namePerro].comida);
+//          }   
+//     }
+// }
