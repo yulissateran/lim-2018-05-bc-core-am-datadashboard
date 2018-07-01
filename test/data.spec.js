9427dad14972fd48c86ed0f02169f4776d30d4ba
yulissateran <yulissa.lteran@gmail.com>
2018-07-01 11:13:47 -0500

    subiendo errores de margenes corregidoss

--- test/data.spec.js
+++ test/data.spec.js
@@ -76,37 +76,225 @@ describe('data', () => {
   });
 
   describe('sortUsers(users, orderBy, orderDirection)', () => {
+    const studentA =  {
+                   id: '61jxyd8IPofBsojdDhJvVvg3p3c2',
+                  signupCohort: 'lim-2018-03-pre-core-pw',
+                  timezone: 'America/Lima',
+                  name: "Zarela Zanabria",
+                   locale: 'es-ES',
+                  role: 'student',
+                  stats: { 
+                    reads:{
+                      total: 10,
+                      completed: 5,
+                      percent: 50
+                    },
+                   quizzes: {
+                    total: 10,
+                    completed: 5,
+                    percent: 50,
+                    scoreSum: 50,
+                    scoreAvg: 10
+                  },
+                   percent: 50,
+                   exercises:  {
+                    total: 10,
+                    completed: 5,
+                    percent: 50
+                  }, } }
+     const studentB = {
+                id: '63u5rmTzG1Ykkl7fSe3pXNegXjx1',
+                name: "Melodie Ayala",
+                locale: 'es-PE',
+                signupCohort: 'lim-2018-03-pre-core-pw',
+                 timezone: 'America/Lima',
+                role: 'student',
+               stats:{ 
+                reads: {
+                  total: 10,
+                  completed: 10,
+                  percent: 100
+                },
+                quizzes: {
+                  total: 10,
+                  completed: 10,
+                  percent: 100,
+                  scoreSum: 1000,
+                  scoreAvg: 100
+                },
+                percent: 100,
+                exercises:{
+                  total: 10,
+                  completed: 10,
+                  percent: 100
+                }, } }                     
+ const studentC = { 
+               id: '6I84bp6FO4OHjE6iiQxBfUutd1w2',
+                signupCohort: 'lim-2018-03-pre-core-pw',
+                timezone: 'America/Lima',
+                name: "Ailim Moscoso",
+                locale: 'es-PE',
+                role: 'student',
+                stats:{
+                  reads: {
+                    total: 10,
+                    completed: 0,
+                    percent: 0
+                  },
+                  quizzes: {
+                    total: 10,
+                    completed: 0,
+                    percent: 0,
+                    scoreSum: 0,
+                    scoreAvg: 0
+                  },
+                  percent: 0,
+                  exercises:{
+                    total: 10,
+                    completed: 0,
+                    percent: 0
+                  },
+                 } 
+                }
+    let students = [studentA, studentB, studentC];
 
-
-    it('debería retornar arreglo de usuarios ordenado por nombre ASC');
-    let orderBy='name';
-    let orderDirection='ASD';
-    let students=[{ name: "Amelia"},{ name: "Zarela"},{name: "Leydy  peralta"}]
-    assert.equal(sortUsers(students,orderBy,orderDirection),[{ name: "Amelia"},{name: "Leydy  peralta"},{ name: "Zarela"}])
-    it('debería retornar arreglo de usuarios ordenado por nombre DESC');
-    it('debería retornar arreglo de usuarios ordenado por porcentaje general ASC');
-    it('debería retornar arreglo de usuarios ordenado por porcentaje general DESC');
-    it('debería retornar arreglo de usuarios ordenado por ejercicios completados ASC');
-    it('debería retornar arreglo de usuarios ordenado por ejercicios completados DESC');
-    it('debería retornar arreglo de usuarios ordenado por quizzes completados ASC');
-    it('debería retornar arreglo de usuarios ordenado por quizzes completados DESC');
-    it('debería retornar arreglo de usuarios ordenado por score promedio en quizzes completados ASC');
-    it('debería retornar arreglo de usuarios ordenado por score promedio en quizzes completados DESC');
-    it('debería retornar arreglo de usuarios ordenado por lecturas (reads) completadas ASC');
-    it('debería retornar arreglo de usuarios ordenado por lecturas (reads) completadas DESC');
+    it('debería retornar arreglo de usuarios ordenado por nombre ASC',()=>{
+      assert.deepEqual(window.sortUsers(students, "name", "asd"), [studentC,studentB,studentA])
+    });
+    
+    it('debería retornar arreglo de usuarios ordenado por nombre DESC',()=>{
+      assert.deepEqual(window.sortUsers(students, "name", "des"), [studentA,studentB,studentC])
+    });
+    it('debería retornar arreglo de usuarios ordenado por porcentaje general ASC',()=>{
+      assert.deepEqual(window.sortUsers(students, "percent", "asd"), [studentC,studentA,studentB])
+    });
+    it('debería retornar arreglo de usuarios ordenado por porcentaje general DESC',()=>{
+      assert.deepEqual(window.sortUsers(students, "percent", "des"), [studentB,studentA,studentC])
+    });
+    it('debería retornar arreglo de usuarios ordenado por ejercicios completados ASC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "asd"), [studentC,studentA,studentB])
+    });
+    it('debería retornar arreglo de usuarios ordenado por ejercicios completados DESC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "des"), [studentB,studentA,studentC])
+    });
+    it('debería retornar arreglo de usuarios ordenado por quizzes completados ASC',()=>{
+      assert.deepEqual(window.sortUsers(students, "quizzes", "asd"), [studentC,studentA,studentB])
+    });
+    it('debería retornar arreglo de usuarios ordenado por quizzes completados DESC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "des"), [studentB,studentA,studentC])
+    });
+    it('debería retornar arreglo de usuarios ordenado por score promedio en quizzes completados ASC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "asd"), [studentC,studentA,studentB])
+    });
+    it('debería retornar arreglo de usuarios ordenado por score promedio en quizzes completados DESC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "des"), [studentB,studentA,studentC])
+    });
+    it('debería retornar arreglo de usuarios ordenado por lecturas (reads) completadas ASC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "asd"), [studentC,studentA,studentB])
+    });
+    it('debería retornar arreglo de usuarios ordenado por lecturas (reads) completadas DESC',()=>{
+      assert.deepEqual(window.sortUsers(students, "exercises", "des"), [studentB,studentA,studentC])
+    });
 
   });
 
   describe('filterUsers(users, filterBy)', () => {
 
-    it('debería retornar nuevo arreglo solo con usuarios con nombres que contengan string (case insensitive)');
+    it('debería retornar nuevo arreglo solo con usuarios con nombres que contengan string (case insensitive)',()=>{
+      let array = [{
+        "id":"LZZiC91B4NddpaPTBJ1XpT9Ox8V2",
+        "name":"Dorelly","locale":"es-PE",
+        "timezone":"America/Lima","role":"student"},
+        {"id":"mIyuhjFX4uhASyDcWReNE5dcd2I2",
+        "github":"",
+        "locale":"es-ES",
+        "timezone":"America/Lima",
+        "name":"Fiorella S",
+        "linkedin":"","role":"student"},
+        {"id":"pHuZDr9WjBV1qrU66QZlq2yhGmC2",
+        "timezone":"America/Lima",
+        "name":"Dory",
+        "locale":"es-PE","role":"student"},
+        {"id":"rosNOO9dNQQDo4TlClcMiFHEIfy2",
+        "name":"Dalia","locale":"es-PE",
+        "timezone":"America/Lima","role":"student"}]
+    const dorelly= [{
+      "id":"LZZiC91B4NddpaPTBJ1XpT9Ox8V2",
+      "name":"Dorelly","locale":"es-PE",
+      "timezone":"America/Lima","role":"student"}
+    ];
+    const dalia = [
+      {"id":"rosNOO9dNQQDo4TlClcMiFHEIfy2",
+    "name":"Dalia","locale":"es-PE",
+    "timezone":"America/Lima","role":"student"}
+  ] 
+      assert.deepEqual(window.filterUsers(array,"Dorelly"), dorelly);
+      assert.deepEqual(window.filterUsers(array,"Dalia"), dalia);
+    });
 
   });
 
   describe('processCohortData({ cohortData, orderBy, orderDirection, filterBy })', () => {
 
-    it('debería retornar arreglo de usuarios con propiedad stats y aplicar sort y filter');
+    it('debería retornar arreglo de usuarios con propiedad stats y aplicar sort y filter',()=>{
+      let options = {
+        cohort:  {
+          id: "lim-2018-03-pre-core-pw",
+          usersCount: 740,
+          publicAdmission: true,
+          start: "2018-03-13",
+          coursesIndex: {
+              intro: {
+                  title: "Introducción a la programación (con JavaScript)"
+              }
+          },
+          end: "2018-03-31"
+      },
+        cohortData : {
+          users: fixtures.users,//array en bruto users
+          progress:fixtures.progress,//objeto en bruto progress
+          coursesIndex : ["intro"]
+        },
+        orderBy:"name",
+        orderDirection:"asc",
+        search : "adriana vizcarra paitán"
+      }
+      describe('processCohortData({ cohortData, orderBy, orderDirection, filterBy })', () => {
+    
+        it('debería retornar arreglo de usuarios con propiedad stats y aplicar sort y filter', () => {
+          assert.deepEqual(window.processCohortData(options),[{
+         id:"MinIWOm1sHOeMguGiQoe1wjqmiC3",
+        locale:"es-PE",
+        timezone: "America/Lima",
+        name:"adriana vizcarra paitán",
+        role:"student",
+        signupCohort:"lim-2018-03-pre-core-pw",
+        stats:{
+        exercises:{
+          total: 2,
+          completed: 2, 
+          percent: 100},
+        percent:100,
+        quizzes:{
+          total: 3, 
+          completed: 3, 
+          percent: 100, 
+          scoreSum: 237, 
+          scoreAvg: 79},
+       reads:{
+         total: 11, 
+         completed: 11, 
+         percent: 100}
+           } }
+          ]);
+        });
+    
+      });
+    
+    });
+    });
 
   });
 
-});
+
+
